18fe60935f2a072c0cdc4b187a10bcf86c501892
JolanEggers <jolan.eggers@gmail.com>
2023-06-18 23:47:21 +0200

    Spatial and temporal resolution implemented, first collision attempt, no pathfinding yet

--- main.py
+++ main.py
@@ -11,59 +11,66 @@ WINDOW_SIZE = (screenWidth, screenHeight)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 font = pygame.font.Font(None, 36)
 
-envWidth = 14
-envHeight = 9
+envWidth = 7
+envHeight = 4.6
+envResolution = 10
+timeResolution = 4.0
 backgroundColor = (255, 255, 255)
+lineThickness = 0.05
 
 
 class Entity:
-    def __init__(self, index=0, target=(0, 0), position=(0, 0)):
+    def __init__(self, index=0, target=(0.0, 0.0), position=(0.0, 0.0), diameter=0.3, speed=1.0):
         self.index = index
         self.target = target
         self.position = position
+        self.diameter = diameter
+        self.speed = speed
 
     def nextStep(self, cellStateInternal):
-        x = self.position[0]
-        y = self.position[1]
-        if y == self.target[1]:
-            if x < self.target[0]:
-                self.position = (self.position[0] + 1, self.position[1])
-        elif x == self.target[0] - 1:
-            if self.target[1] > y:
-                self.position = (self.position[0], self.position[1] + 1)
-            if self.target[1] < y:
-                self.position = (self.position[0], self.position[1] - 1)
-        elif cellStateInternal[x + 1][y] == 0 and environment[x + 1][y] != 2:
-            self.position = (self.position[0] + 1, self.position[1])
+        xIs = self.position[0]
+        yIs = self.position[1]
+        xTarget = self.target[0]
+        yTarget = self.target[1]
+        collision = False
+        for x in range(int(xIs*envResolution - 0.55*envResolution), int(xIs*envResolution + 0.55*envResolution)):
+            for y in range(int(yIs*envResolution - 0.55*envResolution), int(yIs*envResolution + 0.55*envResolution)):
+                if x != int(xIs*envResolution) or y != int(yIs*envResolution):
+                    if cellStateInternal[x][y] != 0:
+                        collision = True
+                        print(f"collision {x} {y}")
+
+        if not collision:
+            if xIs < xTarget:
+                self.position = (xIs + self.speed / timeResolution, yIs)
 
 
-environment = np.zeros((envWidth, envHeight))
-cellState = np.zeros((envWidth, envHeight))
+environment = np.zeros((int(envWidth * envResolution), int(envHeight * envResolution)))
+cellState = np.zeros((int(envWidth * envResolution), int(envHeight * envResolution)))
 entities = []
 
-gridSize = 0.04
-timeStep = 0.3
+gridSize = 0.08
 
 
 def initEnv():
-    for x in range(envWidth):
-        for y in range(envHeight):
-            if y != 4:
-                if x % 2 == 0:
+    for x in range(int(envWidth * envResolution)):
+        for y in range(int(envHeight * envResolution)):
+            if int(2 * y / envResolution) != 4:
+                if int(2 * x / envResolution) % 2 == 0:
                     environment[x][y] = 1
-            if x == 0 or x == envWidth - 1:
+            if x < 0.5 * envResolution or x > (envWidth - 0.5) * envResolution - 1:
                 environment[x][y] = 2
-            if y == 0 or y == envHeight - 1:
+            if y < 0.5 * envResolution or y > (envHeight - 0.5) * envResolution - 1:
                 environment[x][y] = 2
-    # entities.append(Entity(index=1, target=(11, 1), position=(1, 4)))
-    # entitiesToCellState()
+    entities.append(Entity(index=1, target=(5.5, 0.5), position=(0.75, 2.25)))
+    entitiesToCellState()
 
 
 def entitiesToCellState():  # writes the index of the entity in the corresponding cell
     global cellState
-    cellState = np.zeros((envWidth, envHeight))  # clear
+    cellState = np.zeros((int(envWidth * envResolution), int(envHeight * envResolution)))  # clear
     for entity in entities:
-        cellState[entity.position[0]][entity.position[1]] = entity.index
+        cellState[int(entity.position[0] * envResolution)][int(entity.position[1] * envResolution)] = entity.index
 
 
 def nextStep():  # check for only one step forward possible
@@ -74,21 +81,28 @@ def nextStep():  # check for only one step forward possible
 
 def showSpace():
     screen.fill(backgroundColor)
-    for x in range(envWidth):
-        for y in range(envHeight):
+    for x in range(int(envWidth * envResolution)):
+        for y in range(int(envHeight * envResolution)):  # loops for environment
             cellColor = (125, 125, 125)
             if environment[x][y] == 1:  # seat
                 cellColor = (0, 0, 255)
             if environment[x][y] == 2:  # wall
                 cellColor = (125, 0, 0)
             pygame.draw.rect(screen, cellColor, (
-                screenWidth * 0.1 + x * screenWidth * gridSize, screenHeight * 0.1 + y * screenWidth * gridSize,
-                screenWidth * gridSize * 0.95, screenWidth * gridSize * 0.95))
+                screenWidth * 0.1 + x * screenWidth * gridSize / envResolution,
+                screenHeight * 0.1 + y * screenWidth * gridSize / envResolution,
+                screenWidth * gridSize * (1 - lineThickness) / envResolution,
+                screenWidth * gridSize * (1 - lineThickness) / envResolution))
+
+    for x in range(int(envWidth * envResolution)):
+        for y in range(int(envHeight * envResolution)):  # loops for entities
             if cellState[x][y] > 0:  # passanger
                 cellColor = (245, 245, 220)
-                pygame.draw.circle(screen, cellColor, (screenWidth * 0.1 + (x + 0.5) * screenWidth * gridSize,
-                                                       screenHeight * 0.1 + (y + 0.5) * screenWidth * gridSize),
-                                   screenWidth * gridSize * 0.95 * 0.5)
+                diameter = entities[int(cellState[x][y]) - 1].diameter * screenWidth * gridSize * 0.95 * 0.5
+                pygame.draw.circle(screen, cellColor,
+                                   (screenWidth * 0.1 + (x + 0.5) * screenWidth * gridSize / envResolution,
+                                    screenHeight * 0.1 + (y + 0.5) * screenWidth * gridSize / envResolution),
+                                   diameter)
     pygame.display.flip()
 
 
@@ -103,23 +117,12 @@ def main():
     while running:
         current_time = time.time()
         elapsed_time = current_time - start_time
-        if elapsed_time >= timeStep:  # 50 steps per second
+        if elapsed_time >= 1 / timeResolution:  # 50 steps per second
             start_time = current_time
-            nextStep()
-            if currentStepNumber % 3 == 0 and nextSeatY != 4:
-                print(nextSeatX)
-                entities.append(Entity(index=1, target=(nextSeatX, nextSeatY), position=(1, 4)))
-                nextSeatX = nextSeatX - 2
-                if nextSeatX < 1:
-                    nextSeatX = 12
-                    if nextSeatY < 4:
-                        nextSeatY = nextSeatY + 1
-                        if nextSeatY == 4:
-                            nextSeatY = 7
-                    else:
-                        nextSeatY = nextSeatY - 1
-
+            if currentStepNumber%4*timeResolution==3*timeResolution:
+                entities.append(Entity(index=len(entities)+1, target=(5.5, 0.5), position=(0.75, 2.25)))
                 entitiesToCellState()
+            nextStep()
             currentStepNumber = currentStepNumber + 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
